49b4a4ba5430d6222ce19e680dcafe7710f09d22
Gersi365 <Gersi365@outlook.com>
2026-09-10 20:07:07 +0200

    stage12: add audio decoder provenance placeholder

---
+++ app/src/main/java/app/ownplay/mobile/playback/ThirdPartyAudioDecoderInfo.kt
@@ -0,0 +1,8 @@
+package app.ownplay.mobile.playback
+
+internal object ThirdPartyAudioDecoderInfo {
+    const val MEDIA3_VERSION = "1.11.0"
+    const val MEDIA3_COMMIT = "2bc207851df311340767e913931ca7b28cab1794"
+    const val FFMPEG_VERSION = "6.0"
+    const val FFMPEG_COMMIT = "ea3d24bbe3c58b171e55fe2151fc7ffaca3ab3d2"
+}
