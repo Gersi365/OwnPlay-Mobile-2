3ec12c15e0c9f18a71be5d8e6592f317a99ae5b0
Gersi365 <Gersi365@outlook.com>
2026-09-17 16:39:28 +0200

    Wire source repository lifecycle

--- app/src/main/java/app/ownplay/mobile/core/OwnPlayServices.kt
+++ app/src/main/java/app/ownplay/mobile/core/OwnPlayServices.kt
@@ -7,15 +7,18 @@ import app.ownplay.mobile.data.security.CredentialStore
 import app.ownplay.mobile.data.security.KeystoreCredentialStore
 import app.ownplay.mobile.sources.data.OkHttpProviderTransport
 import app.ownplay.mobile.sources.data.ProviderTransport
+import app.ownplay.mobile.sources.data.SourceRepositoryImpl
 import app.ownplay.mobile.sources.data.m3u.M3uClient
 import app.ownplay.mobile.sources.data.m3u.OkHttpM3uClient
 import app.ownplay.mobile.sources.data.xtream.OkHttpXtreamClient
 import app.ownplay.mobile.sources.data.xtream.XtreamClient
+import app.ownplay.mobile.sources.domain.SourceRepository
 
 class OwnPlayServices private constructor(
     val database: OwnPlayDatabase,
     val activeSourcePreferences: ActiveSourcePreferences,
     val credentialStore: CredentialStore,
+    val sourceRepository: SourceRepository,
     val providerTransport: ProviderTransport,
     val m3uClient: M3uClient,
     val xtreamClient: XtreamClient,
@@ -23,11 +26,20 @@ class OwnPlayServices private constructor(
     companion object {
         fun create(context: Context): OwnPlayServices {
             val applicationContext = context.applicationContext
+            val database = OwnPlayDatabase.create(applicationContext)
+            val activeSourcePreferences = ActiveSourcePreferences(applicationContext)
+            val credentialStore = KeystoreCredentialStore(applicationContext)
             val transport = OkHttpProviderTransport()
             return OwnPlayServices(
-                database = OwnPlayDatabase.create(applicationContext),
-                activeSourcePreferences = ActiveSourcePreferences(applicationContext),
-                credentialStore = KeystoreCredentialStore(applicationContext),
+                database = database,
+                activeSourcePreferences = activeSourcePreferences,
+                credentialStore = credentialStore,
+                sourceRepository = SourceRepositoryImpl(
+                    sourceDao = database.sourceDao(),
+                    refreshStateDao = database.refreshStateDao(),
+                    activeSourceStore = activeSourcePreferences,
+                    credentialStore = credentialStore,
+                ),
                 providerTransport = transport,
                 m3uClient = OkHttpM3uClient(transport),
                 xtreamClient = OkHttpXtreamClient(transport),
